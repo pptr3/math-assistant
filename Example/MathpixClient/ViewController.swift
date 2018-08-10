a794db8a6e4387d8f167e89d46f058fb136a2be9
Petru Potrimba <petru.potrimba@studio.unibo.it>
2018-08-10 17:47:13 +0200

    delete old view

---
+++ Example/MathpixClient/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  MathpixClient_Example
+//
+//  Created by Valerio Potrimba on 10/08/2018.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBAction func hh(_ sender: Any) {
+        print("o2")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
