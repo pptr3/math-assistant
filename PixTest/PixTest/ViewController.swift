1627004e51f3201d8c9c2240aaaa16db1c6c8e48
Petru Potrimba <petru.potrimba@studio.unibo.it>
2018-08-11 13:22:19 +0200

    edit .gitignore and add PixTest

---
+++ PixTest/PixTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PixTest
+//
+//  Created by Valerio Potrimba on 11/08/2018.
+//  Copyright © 2018 Petru Potrimba. All rights reserved.
+//
+
+import UIKit
+//import MathpixClient
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
