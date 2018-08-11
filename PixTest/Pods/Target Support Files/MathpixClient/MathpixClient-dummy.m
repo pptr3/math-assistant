b6965a706482730f815636328d85d762705ec4f0
Petru Potrimba <petru.potrimba@studio.unibo.it>
2018-08-11 13:41:28 +0200

    fix bug01

--- PixTest/Pods/Target Support Files/MathpixClient/MathpixClient-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MathpixClient : NSObject
-@end
-@implementation PodsDummy_MathpixClient
-@end
--- PixTest/Pods/Target Support Files/Pods-PixTest/Pods-PixTest-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PixTest : NSObject
-@end
-@implementation PodsDummy_Pods_PixTest
-@end
