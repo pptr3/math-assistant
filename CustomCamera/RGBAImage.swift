630fe91526d20f58a0acdd910a7ad7a06961410c
Petru Potrimba <petru.potrimba@studio.unibo.it>
2018-08-17 13:24:47 +0200

    add RGBAImage class and test it

---
+++ CustomCamera/RGBAImage.swift
@@ -0,0 +1,30 @@
+//
+//  RGBAImage.swift
+//  CustomCamera
+//
+//  Created by Valerio Potrimba on 17/08/2018.
+//  Copyright © 2018 Brian Advent. All rights reserved.
+//
+
+import UIKit
+
+class RGBAImage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
