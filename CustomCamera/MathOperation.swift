399325f1907271bd687d60e815cbb9528e02fe1b
Petru Potrimba <petru.potrimba@studio.unibo.it>
2018-08-21 17:16:43 +0200

    create math operation struct

---
+++ CustomCamera/MathOperation.swift
@@ -0,0 +1,18 @@
+//
+//  MathOperation.swift
+//  CustomCamera
+//
+//  Created by Valerio Potrimba on 21/08/2018.
+//  Copyright © 2018 Brian Advent. All rights reserved.
+//
+
+import Foundation
+
+struct MathOperation {
+    var operation: String
+    var width: Int
+    var height: Int
+    var x: Int
+    var y: Int
+    var isCorrect: Bool
+}
